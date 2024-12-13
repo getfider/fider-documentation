a921d715855e87f1af25ab628ded74fb68f85696
Matt Roberts <roberts.mattroberts@gmail.com>
2024-12-13 16:03:18 +0000

    Hard-coding the baseUrl to see if that helps

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 import "dotenv/config";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-const baseUrl = process.env.BASE_URL || "/";
+// const baseUrl = process.env.BASE_URL || "/";
 
 const config: Config = {
   title: "Fider Docs",
@@ -15,7 +15,7 @@ const config: Config = {
   url: "https://fider.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: baseUrl,
+  baseUrl: "/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
