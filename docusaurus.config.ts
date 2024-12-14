e7dad8ef9cbf8eba48928f57af0c9054c8ddb7be
Matt Roberts <roberts.mattroberts@gmail.com>
2024-12-14 22:15:13 +0000

    Back with the baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
   url: "https://fider.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  trailingSlash: true,
+  baseUrl: "/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
